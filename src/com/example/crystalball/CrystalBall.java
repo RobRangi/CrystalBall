3a2dda2ae6b3627139c66457f08d34eff2ab6050
Robert Rangi <robert_rangi@xtra.co.nz>
2013-07-16 21:48:04 +1200

    Crystal Ball 1.0
    The first release of my first Android app, Crystal Ball; loosely based
    on the Magic 8-Ball. Ask it a question, shake the device and await your
    answer!

---
+++ src/com/example/crystalball/CrystalBall.java
@@ -0,0 +1,38 @@
+package com.example.crystalball;
+
+import java.util.Random;
+
+public class CrystalBall {
+
+	public String[] mAnswers = {
+			"Don't count on it",
+			"Outlook is not good",
+			"Signs point to yes",
+			"Cannot predict now",
+			"Yes",
+			"I'm thinking .. no",
+			"Reply hazy, try again",
+			"Very doubtful",
+			"Outlook good",
+			"The future is unclear",
+			"Yes definitely",
+			"Concentrate and ask again",
+			"Without a doubt",
+			"You may rely on it",
+			"Most likely",
+			"It is certain",
+			"Now is not the time",
+			"As I see it yes",
+			"It is decidedly so",
+			"My sources say no"
+	};
+
+	
+	public String getAnAnswer() {
+		
+		Random randomGenerator = new Random();
+		
+		return mAnswers[randomGenerator.nextInt(mAnswers.length)];
+	}
+	
+}
